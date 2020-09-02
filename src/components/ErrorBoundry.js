b39dc5ecc52040b29c8d2e37bb1d9ce534a74467
JhonzDEV <70487329+jhonzdev@users.noreply.github.com>
2020-09-02 22:49:08 +0800

    update all

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+   constructor(props){
+      super(props)
+      this.state = {
+         hasError: false
+      }
+   }
+
+   componentDidCatch(error, info){
+      this.setState({ hasError: true })
+   }
+
+   render(){
+      if (this.state.hasError){
+         return <h1>Opps. thats not goods</h1>
+      }
+
+      return this.props.children;
+   }
+
+}
+
+export default ErrorBoundry;
