4d83af1adaa69de10e25e065f3010a05a8e9817d
JhonzDEV <70487329+jhonzdev@users.noreply.github.com>
2020-09-03 01:38:46 +0800

    update api http to https

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
    }
 
    componentDidMount() {
-      fetch('http://jsonplaceholder.typicode.com/users')
+      fetch('https://jsonplaceholder.typicode.com/users')
          .then(response => response.json())
          .then(users => this.setState({ robots: users }));
    }
